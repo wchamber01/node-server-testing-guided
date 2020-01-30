eaaacf434359f50fb77fa7b21cd282f50423dd54
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-30 10:40:52 +0100

    wip

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,4 +5,19 @@ describe('server.js module', () => {
   it('has the right environment for DB_ENV', () => {
     expect(process.env.DB_ENV).toBe('testing')
   })
+
+  describe('up endpoint', () => {
+
+    it('returns a 200 OK async/await', async () => {
+      const res = await request(server).get('/')
+    })
+
+    it('returns a 200 OK ES6 promises', () => {
+
+    })
+
+    it('returns a 200 OK supertest', () => {
+
+    })
+  })
 })
